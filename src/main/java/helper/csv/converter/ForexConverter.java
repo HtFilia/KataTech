1e6ea4df7b285c46644e46f47b5f43f1b277ccaa
HtFilia <htfilia@gmail.com>
2022-03-08 18:23:53 +0100

    [CSV] Forex correctly configured

--- src/main/java/helper/csv/converter/ForexConverter.java
+++ src/main/java/helper/csv/converter/ForexConverter.java
@@ -1,2 +1,27 @@
-package helper.csv.converter;public class ForexConverter {
+package helper.csv.converter;
+
+import model.forex.Currency;
+import model.forex.ForexWrapper;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ForexConverter implements IKataConverter {
+	@Override
+	public ForexWrapper convert(List<String[]> lines) {
+		Map<Pair<Currency, Currency>, Double> conversions = new HashMap<>(lines.size());
+		for (String[] line : lines) {
+			Currency from = Currency.valueOf(line[0]);
+			Currency to = Currency.valueOf(line[1]);
+			Double fx = forexFromString(line[2]);
+			conversions.put(Pair.of(from, to), fx);
+		}
+		return new ForexWrapper(conversions);
+	}
+
+	private Double forexFromString(String forex) {
+		return Double.valueOf(forex.replace("\"", "").replace(",", "."));
+	}
 }
